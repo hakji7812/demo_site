9b55ae688da9c0abdf7b5cd341d0f8c6fe995dfd
hakji7812 <hakji7812@naver.com>
2026-02-14 19:57:34 +0900

    메인수정

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model) {        
-        model.addAttribute("message", "VSCode Spring Boot 웹사이트 develop에서 수정!!!!");
+        model.addAttribute("message", "VSCode Spring Boot 웹사이트 develop에서 수정!!!! 다시 main 수정");
         return "index";
     }
 
