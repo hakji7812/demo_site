e21af2d4075a80dae50fe3098790b0487e2a372b
hakji7812 <hakji7812@naver.com>
2026-02-14 19:41:39 +0900

    데모 커밋

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("message", "VSCode Spring Boot 웹사이트 성공!");
+        return "index";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+}
